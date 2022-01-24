35cf7ed327d08df165db5a0248fe1d7e78fea4ef
sateesh.c <sateesh147852@gmail.com>
2022-01-24 16:11:25 +0530

    all contexts added

--- app/src/main/java/com/coroutines/ui/MainActivity.kt
+++ app/src/main/java/com/coroutines/ui/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.coroutines.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.coroutines.databinding.ActivityMainBinding
 import com.coroutines.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +16,51 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
+    //Main = runs on MainThread
+    //IO = runs on workerThread
+    //Default = runs on background thread. it is used when performing complex operations which is taking more time
+    //Confined
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*GlobalScope.launch(Dispatchers.Main){
+            Log.i(TAG, Thread.currentThread().name)
+            Toast.makeText(this@MainActivity,"dummy text",Toast.LENGTH_SHORT).show()
+            binding.tvText.text = "Dummy Text"
+        }*/
+
+        /*GlobalScope.launch(Dispatchers.IO){
+            Log.i(TAG, Thread.currentThread().name)
+            delay(3000)
+            withContext(Dispatchers.Main){
+                Log.i(TAG, Thread.currentThread().name)
+                Toast.makeText(this@MainActivity,"dummy text",Toast.LENGTH_SHORT).show()
+                binding.tvText.text = "Dummy Text"
+            }
+        }*/
+
+        /*GlobalScope.launch(Dispatchers.Default){
+            Log.i(TAG, Thread.currentThread().name)
+            delay(3000)
+            withContext(Dispatchers.Main){
+                Log.i(TAG, Thread.currentThread().name)
+                Toast.makeText(this@MainActivity,"dummy text",Toast.LENGTH_SHORT).show()
+                binding.tvText.text = "Dummy Text"
+            }
+        }*/
+
+        GlobalScope.launch(Dispatchers.Unconfined){
+            Log.i(TAG, Thread.currentThread().name)
+            delay(3000)
+            withContext(Dispatchers.Main){
+                Log.i(TAG, Thread.currentThread().name)
+                Toast.makeText(this@MainActivity,"dummy text",Toast.LENGTH_SHORT).show()
+                binding.tvText.text = "Dummy Text"
+            }
+        }
+
     }
 }
