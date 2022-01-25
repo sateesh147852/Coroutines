dc67e2046bd19a5b9bc49ed5de4079eed23883d0
sateesh.c <sateesh147852@gmail.com>
2022-01-25 21:56:28 +0530

    coroutineScope added

--- app/src/main/java/com/coroutines/ui/MainActivity.kt
+++ app/src/main/java/com/coroutines/ui/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.coroutines.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.coroutines.databinding.ActivityMainBinding
 import com.coroutines.viewmodel.MainViewModel
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+
+    private val job = Job()
+    private val coroutineIOContext : CoroutineContext = job + Dispatchers.IO
+    private val coroutineMainContext : CoroutineContext = job + Dispatchers.Main
+    private val coroutineDefaultContext : CoroutineContext = job + Dispatchers.Default
+
+    private val coroutineIOScope : CoroutineScope = CoroutineScope(coroutineIOContext)
+    private val coroutineMainScope : CoroutineScope = CoroutineScope(coroutineMainContext)
+    private val coroutineDefaultScope : CoroutineScope = CoroutineScope(coroutineDefaultContext)
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
@@ -21,5 +31,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        coroutineMainScope.launch {
+            Log.i(TAG, "coroutine is running on ${Thread.currentThread().name} ")
+        }
+
+        coroutineIOScope.launch {
+            Log.i(TAG, "coroutine is running on ${Thread.currentThread().name} ")
+        }
+
+        coroutineDefaultScope.launch {
+            Log.i(TAG, "coroutine is running on ${Thread.currentThread().name} ")
+        }
+
     }
 }
