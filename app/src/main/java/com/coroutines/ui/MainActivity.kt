905f11ec1a0e24c602ba943c6cd590aa84377d9a
sateesh.c <sateesh147852@gmail.com>
2022-01-25 19:19:36 +0530

    withContext added

--- app/src/main/java/com/coroutines/ui/MainActivity.kt
+++ app/src/main/java/com/coroutines/ui/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.coroutines.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.coroutines.databinding.ActivityMainBinding
 import com.coroutines.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +19,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        GlobalScope.launch(Dispatchers.Main) {
+
+            val time = measureTimeMillis {
+                val answer1 = withContext(Dispatchers.IO) { doNetworkCall1() }
+                Log.i(TAG, "answer1 is  : $answer1")
+                val answer2 = withContext(Dispatchers.IO) { doNetworkCall2() }
+                Log.i(TAG, "answer2 is  : $answer2")
+            }
+            Log.i(TAG, "Request took $time")
+        }
+
+    }
+
+    private suspend fun doNetworkCall1(): String {
+        delay(3000)
+        return "Answer is one"
+    }
+
+    private suspend fun doNetworkCall2(): String {
+        delay(3000)
+        return "Answer is two"
     }
 }
