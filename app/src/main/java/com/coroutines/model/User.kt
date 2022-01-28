7f2d22a39c421f904d12beef1e4cbcbdcab8f57f
sateesh.c <sateesh147852@gmail.com>
2022-01-28 08:48:10 +0530

    runCatching and runMapping added

---
+++ app/src/main/java/com/coroutines/model/User.kt
@@ -0,0 +1,3 @@
+package com.coroutines.model
+
+data class User(val name : String,val age : Int)
--- app/src/main/java/com/coroutines/ui/MainActivity.kt
+++ app/src/main/java/com/coroutines/ui/MainActivity.kt
@@ -1,38 +1,127 @@
 package com.coroutines.ui
 
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.coroutines.adapter.RetroAdapter
 import com.coroutines.databinding.ActivityMainBinding
-import com.coroutines.model.RetroPhoto
+import com.coroutines.model.User
 import com.coroutines.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
-        viewModel.getPhotos().observe(this, {
-            updateRecyclerView(it)
-        })
-
-        viewModel.getError().observe(this, {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
+        //runJobOne()
+        //runJobTwo()
+        //runJobThree()
+        runJobFour()
     }
 
-    private fun updateRecyclerView(list: List<RetroPhoto>) {
-        binding.rvItems.layoutManager = LinearLayoutManager(this)
-        binding.rvItems.adapter = RetroAdapter(list)
+    private fun runJobOne() {
+        runCatching {
+            Log.i(TAG, "onCreate: ${Thread.currentThread().name}")
+            getRandomNumber()
+        }.onSuccess {
+            Log.i(TAG, "$it")
+        }.onFailure {
+            Log.i(TAG, "${it.message}")
+        }
+    }
+
+    private fun runJobTwo() {
+        val result: Result<Int> = runCatching {
+            getRandomNumber()
+        }
+        if (result.isSuccess) {
+            result.map {
+                Log.i(TAG, "$it")
+            }
+
+        } else {
+            result.onFailure {
+                Log.i(TAG, "${it.message} odd number")
+
+            }
+        }
+    }
+
+    private fun runJobThree(){
+
+        val result : Result<Int> = runCatching {
+            getRandomNumber()
+        }
+        if (result.isSuccess){
+
+            Log.i(TAG, "newResult: onSuccess")
+          val newResult: Result<String> =   result.mapCatching {
+                it.toString()
+            }
+
+            if (newResult.isSuccess){
+                newResult.onSuccess {
+                    Log.i(TAG, "newResult: $it ${it.length}")
+                }
+                newResult.onFailure {
+                    Log.i(TAG, "newResult: ${it.message}")
+                }
+            }
+
+        }
+
+        result.onFailure {
+            Log.i(TAG, "${it.message}")
+        }
+
+    }
+
+    private fun runJobFour() {
+
+        val result = kotlin.runCatching {
+            getRandomNumber()
+        }
+        if (result.isSuccess){
+            Log.i(TAG, "success")
+            result.onSuccess {
+
+                val newResult: Result<User> = result.mapCatching {
+                    User("Sateesh",it)
+                }
+
+                if (newResult.isSuccess){
+
+                    newResult.map {
+                        Log.i(TAG, "$it")
+                    }
+
+                }
+                else{
+                    newResult.onFailure {
+                        Log.i(TAG, "${it.message}")
+                    }
+                }
+
+            }
+        }
+        else{
+            result.onFailure {
+                Log.i(TAG, "${it.message}")
+            }
+        }
+
+    }
+
+    private fun getRandomNumber(): Int {
+        val randomNumber = (1..20).shuffled().first()
+        if (randomNumber % 2 == 0)
+            return randomNumber
+        else
+            throw Exception("The random number is odd.")
     }
 }
